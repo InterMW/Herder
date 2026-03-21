7d0b131f5493cb2706202db58f0a9a2737cb7d4d
Joseph-Melberg <josephbmelbergthird@outlook.com>
2026-03-21 15:29:52 +0000

    Maybe this will help?

--- Application/Program.cs
+++ Application/Program.cs
@@ -5,6 +5,7 @@ internal class Program
 {
     private static async Task Main(string[] args)
     {
+        ThreadPool.SetMinThreads(8,8);
         await MelbergHost
             .CreateHost<AppRegistrator>()
             .DevelopmentPasswordReplacement("Rabbit:ClientDeclarations:Connections:0:Password", "rabbit_pass")
